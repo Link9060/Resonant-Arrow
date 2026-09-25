272b68a88d8e52bdf51d4376f9fa3d3b3e2f1e23
Lil Boy <153758512+Link9060@users.noreply.github.com>
2026-09-25 14:53:06 -0500

    Load rebuilt ARROW experience styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { ArrowExperience } from './ArrowExperience';
+import { CinematicApp } from './CinematicApp';
 import './experience.css';
 
-createRoot(document.getElementById('root')).render(<ArrowExperience />);
+createRoot(document.getElementById('root')).render(<CinematicApp/>);
