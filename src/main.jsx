9fcfef365b5f34e77028ed7df56e9f6579459ef4
Lil Boy <153758512+Link9060@users.noreply.github.com>
2026-09-25 14:39:37 -0500

    Switch ARROW to clean rebuilt cinematic experience

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { CinematicApp } from './CinematicApp';
-import './style.css';
-createRoot(document.getElementById('root')).render(<CinematicApp />);
+import { ArrowExperience } from './ArrowExperience';
+import './experience.css';
+
+createRoot(document.getElementById('root')).render(<ArrowExperience />);
